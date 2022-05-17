a356304bb1c80be3b31c76a929759d870e033142
sanjay7900 <singhsanjay790043@gmail.com>
2022-05-17 14:58:26 +0530

    commit the file

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoOfCSharp
+{
+    internal class Program
+    {
+        public Program()
+        {
+
+        }
+        ~Program()
+        {
+
+        }
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("hello i m demo");
+
+            int num1 = 10;
+            int num2 = 10;
+            int num3 = num1 + num2;
+            Console.WriteLine(num3);
+            PerformOOPs obj = new PerformOOPs();
+            Console.WriteLine(obj.sum(12, 39));
+            Console.WriteLine(obj.sum(23.9, 39.3));
+            //Program b=new Program();
+            obj.JustOverRide();
+
+            Console.ReadKey();
+        }
+    }
+}
